1be616ea73bdb3a595be79bbe91ebf9e62c3dbf9
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-05-31 13:44:01 +0000

    Add constants file

---
+++ lib/constants.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+const kbackground = Color(0xFF232f38);
+const kRed = Color(0xFFff403c);
+const kOrange = Color(0xFFfd9831);
+const kYellow = Color(0xFFfdeb5e);
+const kGreen = Color(0xFF32af5a);
+const kGreen2 = Color(0xFF009586);
+const kBlue = Color(0xFF0096e7);
+const kPurple = Color(0xFFa326a6);
+
+// const List<TuneModel> kTunesList = [
+//   TuneModel(tune: 'tunes/note1.wav', color: kRed,),
+//   TuneModel(tune: 'tunes/note2.wav', color: kOrange,),
+//   TuneModel(tune: 'tunes/note3.wav', color: kYellow,),
+//   TuneModel(tune: 'tunes/note4.wav', color: kGreen,),
+//   TuneModel(tune: 'tunes/note5.wav', color: kGreen2,),
+//   TuneModel(tune: 'tunes/note6.wav', color: kBlue,),
+//   TuneModel(tune: 'tunes/note7.wav', color: kPurple,),
+// ];
--- lib/views/tunes_view.dart
+++ lib/views/tunes_view.dart
@@ -5,6 +5,8 @@ class TunesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      
+    );
   }
 }
